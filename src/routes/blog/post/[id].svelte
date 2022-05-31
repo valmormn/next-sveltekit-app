620f7d75e60edfe0eeb6111d5e42403c2985f8e4
Valmor <valmormn@gmail.com>
2022-05-31 07:42:06 -0300

    blog update

--- src/routes/blog/post/[id].svelte
+++ src/routes/blog/post/[id].svelte
@@ -1,5 +1,8 @@
 <script>
   import { onMount } from "svelte";
+  import { base, assets } from "$app/paths";
+  import { page } from '$app/stores';
+
 
   // populated with data from the endpoint
   export let id;
@@ -7,10 +10,18 @@
   onMount(async () => {
     // const response = await fetch(`/api/blog/post/${id}`);
     // const data = await response.json();
-    console.log(id);
+    // console.log(base);
+    // console.log(assets);
+    console.log($page);
+    console.log($page.params.id);
+
     // // populate the data
     // id = data.id;
+
+    // const foo = url.searchParams.get('foo');
   });
 </script>
 
-<h1>{id}</h1>
+<!-- <h1>{id}</h1> -->
+<!-- <h1>{$path}</h1> -->
+
